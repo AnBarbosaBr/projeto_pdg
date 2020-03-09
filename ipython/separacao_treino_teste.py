862c8ae7332a3cdc5bb4927d90036b7ecaf0a42a
André Barbosa <an.barbosa.br@gmail.com>
2020-03-08 21:55:13 -0300

    Módulo usado para separar dados de teste e treinamento.

---
+++ ipython/separacao_treino_teste.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import sklearn.model_selection
+
+SEED = 42
+TRAIN_SIZE = 0.7
+
+data = pd.read_csv("../data/car_insurance_claim.csv")
+
+train_data, test_data = sklearn.model_selection.train_test_split(data, train_size = TRAIN_SIZE, random_state = SEED)
+
+train_data.to_csv("../data/car_insurance_clain_train.csv")
+test_data.to_csv("../data/car_insurance_clain_test.csv")
+
+
+    

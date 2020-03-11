26dea3b9831daae3c07db25fc488cc6f9b603446
BarbAn01 <Andre.Barbosa@lexisnexisrisk.com>
2020-03-11 09:58:22 -0300

    Corrigido output dos testes para que não adicione um index.

--- ipython/separacao_treino_teste.py
+++ ipython/separacao_treino_teste.py
@@ -8,8 +8,8 @@ data = pd.read_csv("../data/car_insurance_claim.csv")
 
 train_data, test_data = sklearn.model_selection.train_test_split(data, train_size = TRAIN_SIZE, random_state = SEED)
 
-train_data.to_csv("../data/car_insurance_clain_train.csv")
-test_data.to_csv("../data/car_insurance_clain_test.csv")
+train_data.to_csv("../data/car_insurance_clain_train.csv", index = False)
+test_data.to_csv("../data/car_insurance_clain_test.csv", index = False)
 
 
     
